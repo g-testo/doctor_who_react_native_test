05f440ce509e1940abef6f743b50af2d371356e1
Gregory Testo <cryptomonger@hotmail.com>
2017-09-15 12:20:34 -0700

    cleaned code

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, ScrollView, Image, Text, Button, View } from 'react-native';
+import { StyleSheet, ScrollView, Image, Text, Button, View, } from 'react-native';
 
+export default class HomeScreen extends React.Component {
 
-export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,53 +11,51 @@ export default class App extends React.Component {
     }
   }
 
-componentDidMount() {
-  var url = 'http://localhost:3001/api/v1/doctors.json'
-  fetch(url)
-    .then((response) => response.json())
-    .then((responseJson) => {
-      const doctorArr = responseJson.map((doctor) => {
-        return {
-          id: doctor.id,
-          name: doctor.name,
-          image_url: 'http://localhost:3001/doctors/' + doctor.image_url,
-          actor_image_url: doctor.actor_image_url,
-          actor_name: doctor.actor_name,
-          category: doctor.category,
-          companions: doctor.companions,
-          episodes: doctor.episodes,
-          species: doctor.species
-        }
+  componentDidMount() {
+    var url = 'http://localhost:3001/api/v1/doctors.json'
+    fetch(url)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        const doctorArr = responseJson.map((doctor) => {
+          return {
+            id: doctor.id,
+            name: doctor.name,
+            image_url: 'http://localhost:3001/doctors/' + doctor.image_url,
+            actor_image_url: doctor.actor_image_url,
+            actor_name: doctor.actor_name,
+            category: doctor.category,
+            companions: doctor.companions,
+            episodes: doctor.episodes,
+            species: doctor.species
+          }
+        })
+        this.setState({
+          isLoading: false,
+          doctorArr
+        });
       })
-      this.setState({
-        isLoading: false,
-        doctorArr
+      .catch((error) => {
+        console.error(error);
       });
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-}
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <ScrollView>
-            <Text style={{fontSize:36}}>Dr Who Images</Text>
-                {this.state.doctorArr.map(doctor =>
-                  { return (
-                    <View key={doctor.id}>
-                      <Text style={{fontSize:36}}>{doctor.actor_name}</Text>
-                      <Image style={{width: 350, height: 350}} source={{uri: doctor.image_url}} />
-                    </View>
-                  )}
-                )}
-        </ScrollView>
-      </View>
-    );
   }
-}
+    render() {
+      return (
+        <View style={styles.container}>
+          <ScrollView>
+              <Text style={{fontSize:36}}>Dr Who Images</Text>
+                  {this.state.doctorArr.map(doctor =>
+                    { return (
+                      <View key={doctor.id}>
+                        <Text style={{fontSize:36}}>{doctor.actor_name}</Text>
+                        <Image style={{width: 350, height: 350}} source={{uri: doctor.image_url}} />
+                      </View>
+                    )}
+                  )}
+          </ScrollView>
+        </View>
+      );
+    }
+  }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
